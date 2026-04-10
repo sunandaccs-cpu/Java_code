d63660f52e32d8db7f3b6daea3686efc6b7d06ac
Sunanda Kumari <sunandaccs@gmail.com>
2026-04-10 21:48:40 +0530

    Create minimum add to make parentheses valid.java

---
+++ minimum add to make parentheses valid.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int ans = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0) {
+                    open--;
+                } else {
+                    ans++;
+                }
+            }
+        }
+
+        return ans + open;
+    }
+}
